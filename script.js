c8b5f138c40b582bcb9fcfb620495d481c17964b
vinu cyril <38722628+vinuman@users.noreply.github.com>
2023-04-08 21:09:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 const button = document.getElementById('btn');
 const age = document.getElementById('age');
 const name = document.getElementById('name');
+const error = document.querySelector('.error')
 
 button.addEventListener('click', ()=>{
 	if(age.value=='' || name.value==''){
-		alert('inputs cannot be empty');
-	}
-	return new Promise((resolve, reject) => {
+			error.innerHTML = `
+       <p>inputs cannot be empty.</p>
+   `;
+	}else{
+		error.innerHTML = '';
+			return new Promise((resolve, reject) => {
 
 		setTimeout(()=>{
 			if(age.value>=18){
@@ -18,4 +22,6 @@ button.addEventListener('click', ()=>{
 		}, 4000)
 		
 	})
-})
+	}
+
+});
